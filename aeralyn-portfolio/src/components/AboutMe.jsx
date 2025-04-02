70bc266a9303e62d36194bbd35fe88a7abacd0e6
Aeralyn <Aeralyn123@gmail.com>
2025-04-02 12:01:27 -0600

    Creating AboutMe.jsx

---
+++ aeralyn-portfolio/src/components/AboutMe.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <h3>Aeralyn Neff</h3>
+
+      <img
+        src="https://drive.google.com/file/d/1KnwEMF9VZw9kz6n003xvZai57meR54Ka/view?usp=drive_link"
+        alt="Photo of myself"
+        style={{ width: "200px", borderRadius: "50%" }}
+      />
+
+      <p>
+        I'm an enthusiastic beginner in web development, currently exploring the
+        world of coding through my first boot camp, and I hope to bring a fresh
+        perspective and a strong drive to learn and grow in the tech industry.
+        By day I work as a team lead at a movie theater, but my true passion
+        lies in building; I love painting, sculpting, cooking and experimenting
+        with different forms of artistic expression. This creative background
+        fuels my excitement for building and designing in the digital space. I'm
+        eager to blend my desire to create with my growing coding knowledge to
+        make innovative and useful web applications.
+      </p>
+
+      <h4>Skills</h4>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>CSS</li>
+        <li>Typescript</li>
+        <li>Python</li>
+      </ul>
+    </section>
+  );
+};
+
+export default AboutMe;
